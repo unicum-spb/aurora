9f6f1025b7b6b46780b32ad15fdd73719aa6344a
AlexQuidditch <alexquidditch@gmail.com>
2019-06-02 15:49:15 +0300

    initial frontend

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="container">
+    <div class="large-12 medium-12 small-12 cell">
+      <label>File
+        <input type="file" id="file" ref="file" @change="onSubmit($event.target.files)" />
+      </label>
+    </div>
+  </div>
+
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+
+  methods: {
+    async onSubmit (files) {
+      console.log(files);
+      
+      const formData = new FormData();
+      for (const file of files) {
+        formData.append('file', file);
+      }
+
+      try {
+        const result = await axios.post( 'http://localhost:3000/upload',
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          }
+        );
+        console.log(result);
+      } catch (error) {
+        console.error(error.response.data);
+      }
+    }
+  }
+};
+
+</script>
