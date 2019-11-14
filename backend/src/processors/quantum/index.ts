71a8982231f4b4694a717aef8f58c7a468ef158e
AlexQuidditch <alexquidditch@gmail.com>
2019-11-14 03:17:09 +0300

    quantum reports parser

---
+++ backend/src/processors/quantum/index.ts
@@ -0,0 +1,129 @@
+import { Scalars, ReportResult, QuantumReportModel } from '@/types';
+
+import * as fs from 'fs';
+import * as JSZip from 'jszip';
+
+import { EnvironmentServiceConstants } from '../../keys';
+
+import { python } from '../../adapters';
+import { Report } from './report';
+
+export class Processor {
+  private file: Express.Multer.File;
+  private names: Array<Scalars['String']>;
+  private extractedFiles: Array<Scalars['String']>;
+  private reports: Array<ReportResult>;
+
+  constructor (file: Express.Multer.File) {
+    this.file = file;
+    this.names = [];
+    this.extractedFiles = [];
+    this.reports = [];
+  }
+
+  getFileList () {
+    return this.extractedFiles;
+  }
+
+  getNameList () {
+    return this.names;
+  }
+
+  buildReports () {
+    const { reports, extractedFiles } = this;
+
+    try {
+      for (const file of extractedFiles) {
+        const report = new Report(file);
+
+        const result = report
+          .extractTitle()
+          .createStructure()
+          .createData()
+          .convertToJSON()
+          .getResult()
+
+        this.reports.push(result);
+      }
+
+      const meta = new Report(extractedFiles[0])
+        .extractMetaInformation()
+        .getMetaData();
+
+      const result: QuantumReportModel = {
+        meta,
+        reports
+      };
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  extractFiles () {
+    const { file } = this;
+
+    return new Promise<Array<Scalars['String']>>((resolve, reject) => {
+      fs.readFile(`${ EnvironmentServiceConstants.TEMP_PATH }/${ file.filename }`, async (err, data) => {
+        if (err) reject(err);
+
+        try {
+          // асинхронно загружаю архив
+          const zip = await JSZip.loadAsync(data);
+          // получаю список имён файлов
+          const files = Object.keys(zip.files);
+
+          // отфильтровываю *.htm файлы
+          this.names = files.filter((file, i) => file.includes('.htm') && i != 0);
+
+          // извлекаю каждый .htm файл как строку
+          // добавляю в массив файлов 
+          for (const fileName of this.names) {
+            // @ts-ignore
+            if (JSZip.support.string) {
+              // @ts-ignore
+              const file: string = await zip.file(fileName).async('string');
+              this.extractedFiles.push(file);
+            }
+          }
+
+          // возвращаю массив файлов в виде строк
+          resolve(this.extractedFiles);
+        } catch (error) {
+          console.error('Quantum: extractFiles -', error);
+          reject(error);
+        }
+      });
+    });
+  }
+
+  async unpackZIP () {
+    const { file } = this;
+
+    try {
+      await new Promise<boolean>((resolve, reject) => {
+        return python({
+          args: [
+            file.path,
+            `${EnvironmentServiceConstants.TEMP_PATH}/${file.filename}`
+          ],
+        })
+          .end(async (err, code, signal) => {
+            if (err) reject(err);
+
+            console.log('The exit code was: ' + code);
+            console.log('The exit signal was: ' + signal);
+            console.log('finished');
+
+            resolve(true);
+          });
+      });
+      
+    } catch (error) {
+      console.error('extractProcess - ', error);
+    }
+
+    return this;
+  }
+}
---
+++ backend/src/processors/quantum/utils.ts
@@ -0,0 +1,54 @@
+import { Scalars } from '@/types';
+
+import dayjs              from 'dayjs';
+import customParseFormat  from 'dayjs/plugin/customParseFormat';
+import cyrillicToTranslit from 'cyrillic-to-translit-js';
+
+import { Sex, SexCyrillic } from '../../types/enums';
+
+dayjs.extend(customParseFormat);
+
+export function normalizeNumber (number: Scalars['String'] = '') {
+  return parseFloat(number.trim().replace(',', '.'));
+}
+
+export function parsePhysique (physique: Scalars['String'] = '') {
+  const height = physique.split(',')[0];
+  const weight = physique.split(',')[1];
+
+  const clearHeight = height.match(/\d/g) || [];
+  const clearWeight = weight.match(/\d/g) || [];
+
+  return {
+    height: Number(clearHeight.join('')),
+    weight: Number(clearWeight.join('')),
+  };
+}
+
+export function parseDate (dateAndTimeString: Scalars['String'] = '') {
+  const date = dateAndTimeString.split(' ')[0];
+  const time = dateAndTimeString.split(' ')[1];
+  const hours   = time.split(':')[0];
+  const minutes = time.split(':')[1];
+  
+  return dayjs(date, 'DD.MM.YYYY')
+    .hour(Number(hours))
+    .minute(Number(minutes))
+    .format();
+}
+
+export function parseName (name: Scalars['String'] = ''): Scalars['String'] {
+  return new cyrillicToTranslit().transform(name);
+}
+
+export function parseSex (sex: Scalars['String']) {
+  if (sex !== SexCyrillic.Male) {
+    return Sex.Female;
+  } else {
+    return Sex.Female;
+  }
+}
+
+export function parseAge (age: Scalars['String'] | number = '') {
+  return Number(age) | 0;
+}
