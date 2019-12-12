4029ad9fe5fa7c803c638c2cd4e9b9fbae50cc6d
AlexQuidditch <alexquidditch@gmail.com>
2019-12-13 02:20:43 +0300

    fix Python cli

--- backend/src/adapters/python.ts
+++ backend/src/adapters/python.ts
@@ -1,10 +1,10 @@
 import { PythonShell, Options } from 'python-shell';
 
-export function python (options: Options) {
+export function python(options: Options) {
   return new PythonShell(
     `${process.cwd()}/dist/scripts/unpacker.py`,
     {
-      pythonPath: 'python3',
+      pythonPath: 'py',
       ...options
     }
   );
